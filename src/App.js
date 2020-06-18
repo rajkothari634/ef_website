785ee87b9af22dc2716f64668a0f115da566c54d
Raj kothari <rkmeansrk39@gmail.com>
2020-06-18 14:11:56 +0530

    update App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/home";
+import BidderPage from "./pages/bidder";
+import DemandPartner from "./pages/demandpartner";
 
 const App = () => {
-  return <HomePage />;
+  return <BidderPage />;
 };
 
 export default App;
