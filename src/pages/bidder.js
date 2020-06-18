a72afd165c42493d634346ab6fcffa2600fa5b35
Raj kothari <rkmeansrk39@gmail.com>
2020-06-18 14:15:56 +0530

    update

--- src/pages/bidder.js
+++ src/pages/bidder.js
@@ -1,58 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "../components/header";
+import { Header } from "../components/header";
 import ContactUs from "../components/contactus";
+import { InfoBlock } from "../components/infoblock";
 
-const BidderPageDiv = styled.div`
-  display: flex;
-  flex: wrap;
-  flex-direction: column;
-  max-width: 100%;
-`;
-const TitleDiv = styled.div`
-  width: 100%;
-  background-color: #211551;
-  padding-top: 150px;
-`;
-const PageHeaderInfo = styled.p`
-  width: 60%;
-  font-size: 40px;
-  color: #ffffff;
-  text-align: center;
-  line-height: 60px;
-  margin-top: 20px;
-  margin-bottom: 60px;
-`;
-const PageHeading = styled.p`
-  width: 90%;
-  font-size: 50px;
-  color: #ffffff;
-  text-align: center;
-  margin-bottom: 45px;
-  @media (min-width: 600px) {
-    font-size: 102px;
-  }
-`;
-const ContactBtn = styled.div`
-    padding-top: 16px;
-    font-size: 24px;
-    width: 240px;
-    text-align: center;
-    height: 44px;
-    padding-right: 60px:
-    padding-left: 60px;
-    color: #ffffff;
-    background-color: #e84039;
-    border-radius: 5px;
-    cursor:pointer;
-    &:hover{
-        border: ${(props) => props.hoverBorder};
-        color: ${(props) => props.hoverFontColor};
-        background-color: #00000000;
-        padding-top:14px;
-        height: 42px;
-    }
-`;
+import {
+  HomePageDiv,
+  PageHeading,
+  TitleDiv,
+  PageHeaderInfo,
+} from "../pages/home";
+
+const BidderPageDiv = HomePageDiv;
+// const TitleDiv = styled.div`
+//   width: 100%;
+//   background-color: #211551;
+//   padding-top: 150px;
+// `;
+// const PageHeaderInfo = styled.p`
+//   width: 60%;
+//   font-size: 40px;
+//   color: #ffffff;
+//   text-align: center;
+//   line-height: 60px;
+//   margin-top: 20px;
+//   margin-bottom: 60px;
+// `;
+// const PageHeading = styled.p`
+//   width: 90%;
+//   font-size: 50px;
+//   color: #ffffff;
+//   text-align: center;
+//   margin-bottom: 45px;
+//   @media (min-width: 600px) {
+//     font-size: 102px;
+//   }
+// `;
 
 const BidderPage = () => {
   return (
@@ -62,12 +45,48 @@ const BidderPage = () => {
         <center>
           <PageHeading>BidMachine Bidder</PageHeading>
           <PageHeaderInfo>
-            Empowering mobile app publishers and advertisers to partner and earn
-            without obstacles.
+            Take advantage of innovative in-app bidding technology with Prebid
+            at the forefront. This solution comes free with all of our
+            programmatic offerings.
           </PageHeaderInfo>
+          <div
+            style={{
+              background: "#000000",
+              width: "50%",
+              height: "400px",
+              marginLeft: "25%",
+              marginRight: "25%",
+              marginBottom: "-100px",
+            }}
+          ></div>
         </center>
       </TitleDiv>
+      <InfoBlock
+        marginTop={"150px"}
+        headColor={"#000000"}
+        infoHead={"What is Prebid Mobile?"}
+        paraBackgroundColor={"#00000000"}
+        animationSide={"right"}
+        paraColor={"#000000"}
+        paraContent={`Prebid sends one request to the server where multiple demand sources are simultaneously asked for bids. Bids return from demand partners and the highest bid wins in less than a second. Prebid can connect you to ad exchanges of your choice as well as traditional set of DSPs.`}
+      />
+      <InfoBlock
+        marginTop={"100px"}
+        headColor={"#000000"}
+        infoHead={"Extended analytics"}
+        paraBackgroundColor={"#00000000"}
+        animationSide={"left"}
+        paraColor={"#000000"}
+        paraContent={`The Prebid solution allows you to gather analytics from SDK bidders as well as DSPs in one spot, eliminating the need for multiple integrations.
+\n
+        One SDK — all major ad exchanges
+        Unified analytics from all bidders\n
+        Transparent auction data + raw data`}
+      />
+      <div style={{ height: "70px" }}></div>
       <ContactUs />
     </BidderPageDiv>
   );
 };
+
+export default BidderPage;
