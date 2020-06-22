33a6f558d689afd07dc54367bb615f85c2da09b4
Raj kothari <rkmeansrk39@gmail.com>
2020-06-22 15:54:45 +0530

    Updates

---
+++ precache-manifest.46b2e0cbd2572c8dda2d116e920b0e9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565d6295b897865ad7813c76ddfb218a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c130e95a510ebf21685",
+    "url": "./static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d9c11090fd012d470925",
+    "url": "./static/css/main.fcdd7088.chunk.css"
+  },
+  {
+    "revision": "6c130e95a510ebf21685",
+    "url": "./static/js/2.702d8f79.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.702d8f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c11090fd012d470925",
+    "url": "./static/js/main.cd16d4f3.chunk.js"
+  },
+  {
+    "revision": "bc1c9ea3d7bdb9cefb1a",
+    "url": "./static/js/runtime-main.2c9801d8.js"
+  },
+  {
+    "revision": "1a68711526aae558cfa396082d99e479",
+    "url": "./static/media/articleimg.1a687115.png"
+  }
+]);
